9fe3ca21f60075f6934a77b1d97300fd85172e6a
Kiran Gautam <kgautam@oceanx.com>
2017-09-21 11:10:03 -0700

    updating few things

--- resources/js/index.js
+++ resources/js/index.js
@@ -4,15 +4,15 @@ function ajaxcall() {
     var message = $('#message').val();
     var link = $('#link').val();
     var oceanx_icon_url = "https://lh3.googleusercontent.com/gQzJTIwghRjVhP3c1hfSHfk1uXcVVeUyTidWxBRsM1JBmrgZgqdFksaQ6L14FemXxJ7o1BBNbxVKCFl0asrE3oUS9xBSxUcUmfKHv1_axZO5jSf_HAXPyz9Rq32EvGXua8KkMZa-7A=w247-h230-no";
+    var god_icon = "https://previews.123rf.com/images/martialred/martialred1511/martialred151100007/47998755-Eye-of-providence-or-all-seeing-eye-of-God-flat-icon-for-apps-and-websites-Stock-Vector.jpg";
     var areEqual = name.toUpperCase() === "OCEANX";
     if (areEqual == true) {
         default_icon = oceanx_icon_url;
     }
-
     var payload = {
         'text': message + " " + link,
         'username': name,
-        'icon_emoji': ':skull:'
+        "icon_url": god_icon
     }
     console.log(payload);
     if (message == "" && name == "") {
@@ -25,6 +25,8 @@ function ajaxcall() {
         $('#name').css('border-color','');
         $('#message').css('border-color','red');
     } else {
+        $('#message').css('border-color','');
+        $('#name').css('border-color','');
         $.ajax({
             type: 'POST',
             url: 'https://hooks.slack.com/services/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',
@@ -44,7 +46,6 @@ function ajaxcall() {
 $('input').click(function() {
     var status = $(this).val();
     if (status == 'on') {
-
         $.ajax({
             url: 'http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1',
             type: 'GET',
@@ -53,8 +54,10 @@ $('input').click(function() {
                 var post = data.shift();
                 console.log(post.title);
                 console.log(post.content);
-                $('#quote-title').text(post.title);
-                $('#quote-content').html(post.content);
+                var quote = post.content;
+                $('#quote').html(quote);
+                $('#quote').show();
+                animateQuote();
 
                 // If the Source is available, use it. Otherwise hide it.
                 if (typeof post.custom_meta !== 'undefined' && typeof post.custom_meta.Source !== 'undefined') {
@@ -66,8 +69,11 @@ $('input').click(function() {
             cache: false
         });
 
-        $('#quote').show();
-        animateQuote();
+        console.log(quote);
+
+        // $('#quote').html("hello");
+        // $('#quote').show();
+        // animateQuote();
     } else {
         $('#quote').hide();
 
