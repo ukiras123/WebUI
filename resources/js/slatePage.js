34bc1329392fcf24b6fbe19d0d71196cca01b368
Kiran Gautam <kgautam@oceanx.com>
2017-10-21 01:41:23 -0700

    saving

--- resources/js/slatePage.js
+++ resources/js/slatePage.js
@@ -20,3 +20,74 @@ jQuery(document).ready(function ($) {
     });
 
 });
+
+
+function clearFields() {
+    $('#branch').val('');
+    $('#email').val('');
+}
+
+function disableFields() {
+    $('#regression-btn').removeAttr("disabled");
+    $('#smoke-test-btn').removeAttr("disabled");
+    $('#api-name').removeAttr("disabled");
+    $('#branch').removeAttr("disabled");
+    $('#email').removeAttr("disabled");
+}
+
+
+function enableFields() {
+    $('#regression-btn').attr("disabled", "disabled");
+    $('#smoke-test-btn').attr("disabled", "disabled");
+    $('#api-name').attr("disabled", "disabled");
+    $('#branch').attr("disabled", "disabled");
+    $('#email').attr("disabled", "disabled");
+}
+
+function hideResponseMessage() {
+    $('#error-div').hide();
+    $('#success-div').hide();
+}
+
+function triggerRegression() {
+    hideResponseMessage();
+    console.log("Inside Ajax")
+
+    var apiName = $('#api-name').find(":selected").val();
+    var branch = $('#branch').val();
+    var url = "http://9a5d5e5d.ngrok.io/automation";
+
+    if (branch == '') {
+        branch = 'integration';
+    }
+    var email = $('#email').val();
+
+    var payload = {
+        "apiName": apiName,
+        "branch": branch,
+        "email": email
+    }
+    console.log(payload);
+
+    disableFields();
+    $.ajax({
+        type: 'POST',
+        header: {'Content-Type': 'application/json', 'Accept': 'application/json'},
+        contentType: 'application/json',
+        url: url,
+        data: JSON.stringify(payload),
+    }).then(function (result) {
+        console.log('success');
+        console.log(result.responseText);
+        $('#success-response').html(result['text']);
+        $('#success-div').show();
+    }, function (error) {
+        console.log('error');
+        console.log(error);
+        $('#error-response').html(error);
+        $('#error-div').show();
+    });
+    enableFields();
+}
+
+
