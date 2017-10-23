8aabb67780f9c0088193659905bc32bdb2a9ffb8
Kiran Gautam <kgautam@oceanx.com>
2017-10-23 10:06:11 -0700

    adding url

--- resources/js/slatePage.js
+++ resources/js/slatePage.js
@@ -78,7 +78,7 @@ function automate(id) {
 
     var apiName = $('#api-name').find(":selected").val();
     var branch = $('#branch').val();
-    var url = "http://5829acf5.ngrok.io/automation";
+    var url = "http://503e2ffd.ngrok.io/automation";
 
     if (branch == '') {
         branch = 'integration';
